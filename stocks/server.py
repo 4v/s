cb7b4cd38549df1575e012e3b79bae456655eb5f
xiaojiao.he <xiaojiao.he@tongdun.cn>
2018-05-31 22:15:23 +0800

    update

--- stocks/server.py
+++ stocks/server.py
@@ -8,17 +8,17 @@ import json
 
 from bottle import route, run, template, request
 
-@route('/stock/<code>')
+@route('/s/<code>')
 def index(code):
     stock = datasource.EastMoney().load_one(code)
     return render_stock(stock)
 
-@route('/fund/<code>')
-def index(code):
+@route('/f/<code>')
+def indexf(code):
     stock = datasource.DayDayFund().load(code)
     return render_stock(stock)
-
-@route('/coin/huobi/<code>')
+#/code/huobi
+@route('/ch/<code>')
 def coin_huobi(code):
     stock = datasource.Huobi().load(code, '5min')
     return render_stock(stock)
@@ -29,6 +29,7 @@ def render_stock(stock):
     m = model.SlopeModel(stock['prices'][-1000:], parameter=parameter)
     m.run()
     chart = m.get_chart()
+    #print(chart)
     return html_chart(chart)
 
 def html_chart(chart):
@@ -37,24 +38,53 @@ def html_chart(chart):
 <html>
 <head>
     <meta charset="utf-8">
-    <script src="https://cdn.bootcss.com/echarts/3.8.5/echarts.min.js"></script>
+    <script src="https://cdn.bootcss.com/echarts/4.1.0/echarts.min.js"></script>
+    <script src="https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js"></script>
+    <script type="text/javascript">
+        function getUrlSCode(){
+            var localurl=window.location.href;  //取得整个地址栏
+            return localurl.substring(localurl.lastIndexOf("/")+1,localurl.length);
+        }
+        var stockCode = getUrlSCode();
+    </script>
 </head>
 <body>
-    <input id="parameter" type="input"/>
+    <div id="sname" align="center"></div>
+    <input id="parameter" type="input" onkeydown="show();"/> 
     <button id="applyButton">修改</button>
-    <div id="main" style="width: 100%%;height:600px;"></div>
+    <div id="main" style="width:800px;height:400px;"></div>
     <script type="text/javascript">
         var myChart = echarts.init(document.getElementById('main'));
         var option = %s;
         myChart.setOption(option);
         var applyButton = document.getElementById('applyButton');
         var parameterInput = document.getElementById('parameter');
+        parameterInput.value = stockCode;
+        //var stockelem = getStockData(stockCode);
+        //$("#sname").html() = stockelem[0];
+
+        function getStockData(stockCodeNum){
+            var param_string = "hq_str_" + stockCodeNum;  
+            var param = eval(param_string);  
+            var elements = param.split(",");
+            return elements;
+        }
         applyButton.onclick = function() {
-            console.log(parameterInput.value);
+            scode = parameterInput.value;
+            if(scode){
+                console.log(scode);
+                window.location.href="/s/"+scode; 
+            }
+        }
+        function show() {
+            var e=window.event||arguments.callee.caller.arguments[0];
+            if(e.keyCode==13){
+                applyButton.onclick();
+            }
         }
     </script>
 </body>
 </html>
 ''' % chart)
 
-run(host='127.0.0.1', port=8080)
+run(host='0.0.0.0', port=8081)
