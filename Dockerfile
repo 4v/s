736beee4482ebe8a7daedc54a7ea1877fd18ab7c
4v <hexiaojiao@gmail.com>
2019-12-13 18:46:13 +0800

    update

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM python:3.7-alpine
+
+RUN sed -i 's/dl-cdn.alpinelinux.org/mirrors.aliyun.com/g' /etc/apk/repositories \
+    && cat /etc/apk/repositories \
+    && echo -e "[global]\nindex-url = http://mirrors.aliyun.com/pypi/simple/\n[install]\ntrusted-host=mirrors.aliyun.com">>/etc/pip.conf 
+RUN apk add --update build-base python-dev && pip install --upgrade pip \
+    && pip install numpy gunicorn pandas
+# RUN apk add --update build-base python python-dev py-pip && pip install --upgrade pip
+
+# https://github.com/9fevrier/docker-python-ta-lib/blob/master/Dockerfile
+COPY deps/ta-lib-0.4.0-src.tar.gz .
+RUN tar xvf ta-lib-0.4.0-src.tar.gz \
+    && cd ta-lib \
+    && sed -i 's/^#define TA_IS_ZERO(v).*$/#define TA_IS_ZERO(v) (((-0.000000000000000001)<v)\&\&(v<0.000000000000000001))/' src/ta_func/ta_utility.h \
+    && sed -i 's/^#define TA_IS_ZERO_OR_NEG(v).*$/#define TA_IS_ZERO_OR_NEG(v) (v<0.000000000000000001)/' src/ta_func/ta_utility.h \
+    && ./configure --prefix=/usr \
+    && make \
+    && make install \
+    && cd .. \
+    && rm -rf ta-lib \
+    && rm ta-lib-0.4.0-src.tar.gz
--- stocks/server.py
+++ stocks/server.py
@@ -6,7 +6,7 @@ import util
 import account
 import json
 
-from bottle import route, run, template, request
+from bottle import app, route, run, template, request
 
 @route('/')
 def indexrt():
@@ -35,88 +35,13 @@ def render_stock(stock):
                          stock['name'], parameter=parameter)
     m.run()
     chart = m.get_chart()
-    #print(chart)
-    return html_chart(chart)
+    return template('index', option=chart)
 
-def html_chart(chart):
-    return template('''
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>爱量化</title>
-    <script src="https://cdn.bootcss.com/echarts/4.1.0/echarts.min.js"></script>
-    <script src="https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js"></script>
-    <script type="text/javascript">
+app = app()
+if __name__ == '__main__':
+    run(app=app, host='0.0.0.0', port=8081)
 
-        var stockCode;
-        var locurl;
-        var urltp;
-        function getUrlSCode(){
-            var localurl=window.location.href;  //取得整个地址栏
-            stockCode=localurl.substring(localurl.lastIndexOf("/")+1,localurl.length);
-            locurl=localurl.substring(0,localurl.lastIndexOf("/"));
-            if(locurl){
-                urltp=locurl.substring(locurl.lastIndexOf("/"),locurl.length);
-            }
-        }
-        getUrlSCode();
-    </script>
-    <style type="text/css">
-        body { margin: 30px; font-family:Didot,"Microsoft YaHei","微软雅黑","Times New Roman",Georgia,Serif }
-        .wt { width:808px;font-size: 13px;}
-    </style>
-</head>
-<body>
-    <div id="sname" align="right" style="border-bottom:solid 1px grey" class="wt">
-    <input id="parameter" type="input" value="sh000001" onkeydown="show();"/> 
-    <button id="applyButton">修改</button></div>
-    <div id="main" style="width:808px;height:400px;"></div>
-    <script type="text/javascript">
-        var myChart = echarts.init(document.getElementById('main'));
-        var option = %s;
-        myChart.setOption(option);
-        var applyButton = document.getElementById('applyButton');
-        var parameterInput = document.getElementById('parameter');
-        parameterInput.value = stockCode;
-        //var stockelem = getStockData(stockCode);
-        //$("#sname").html() = stockelem[0];
-
-        function getStockData(stockCodeNum){
-            var param_string = "hq_str_" + stockCodeNum;  
-            var param = eval(param_string);  
-            var elements = param.split(",");
-            return elements;
-        }
-        applyButton.onclick = function() {
-            scode = parameterInput.value;
-            if(scode){
-                var perfix;
-                if(urltp=="/s"||urltp=="/f"||urltp=="/ch"){
-                    perfix = urltp;
-                }else{
-                    perfix="/s";
-                }
-                window.location.href=perfix+"/"+scode; 
-            }
-        }
-        function show() {
-            var e=window.event||arguments.callee.caller.arguments[0];
-            if(e.keyCode==13){
-                applyButton.onclick();
-            }
-        }
-    </script>
-    <!-- 
-    <div align="center" class="wt"> Q群:243755300 </div>
-    -->
-    <br>
-    <br>
-    <hr>
-    <div align="center" class="wt"> Author：乾朗Jonathan </div>
-    <hr>
-</body>
-</html>
-''' % chart)
-
-run(host='127.0.0.1', port=8081)
+def test():
+    stock = datasource.EastMoney().load_one('sh000001')
+    ctxs = render_stock(stock)
+    print(ctxs)
