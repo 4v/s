393f17c0037ffead2f5b0e730846b43953af19fe
4v <hexiaojiao@gmail.com>
2019-12-19 15:20:56 +0800

    modify conf folder and add shells

---
+++ shells/detect-stocks.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+ function check(){
+    count=`ps -ef |grep $1 |grep -v "grep" |wc -l`
+    #echo $count
+    if [ 0 == $count ];then
+    nohup python3 /root/srcs/s/stocks/$1 &
+    fi
+}
+
+check server.py
