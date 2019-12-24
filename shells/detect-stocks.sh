e2b87583b473b47ce7c5c11140b5a4032f475167
4v <hexiaojiao@gmail.com>
2019-12-24 19:53:21 +0800

    update nginx conf and add system service

--- shells/detect-stocks.sh
+++ shells/detect-stocks.sh
@@ -4,7 +4,7 @@
     #echo $count
     if [ 0 == $count ];then
         # cd /root/srcs/s/stocks/ && nohup python3 $1 &
-        nohup python3 /root/srcs/s/stocks/$1 &
+        nohup python3 /root/srcs/s/stocks/$1 >/root/logs/nohup.out 2>&1 &
     fi
 }
 
--- shells/httpsvc.sh
+++ shells/httpsvc.sh
@@ -1,1 +1,2 @@
-python -m SimpleHTTPServer 8080
+# python2 -m SimpleHTTPServer 8080
+python3 -m http.server 8080
